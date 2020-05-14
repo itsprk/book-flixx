7cb4133e75ffffb12f2dfae83c5877166d440f2d
Pradeep Kushwaha <prk.shine@gmail.com>
2020-05-14 16:09:57 +0530

    Updates

---
+++ precache-manifest.fe325f61e9ec876a4f4980fc3a430786.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b3b39fbd8ef4399009e5d4d6a39bb1",
+    "url": "/book-flixx/index.html"
+  },
+  {
+    "revision": "20a5ed1b532f0e72798e",
+    "url": "/book-flixx/static/css/2.01bbb909.chunk.css"
+  },
+  {
+    "revision": "5352477d839dae707d7c",
+    "url": "/book-flixx/static/css/main.a00742b2.chunk.css"
+  },
+  {
+    "revision": "20a5ed1b532f0e72798e",
+    "url": "/book-flixx/static/js/2.3cf131f8.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/book-flixx/static/js/2.3cf131f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5352477d839dae707d7c",
+    "url": "/book-flixx/static/js/main.d5f37be4.chunk.js"
+  },
+  {
+    "revision": "4ef844f9c33974fb006b",
+    "url": "/book-flixx/static/js/runtime-main.be368036.js"
+  }
+]);
